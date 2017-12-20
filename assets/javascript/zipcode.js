48ddb3c318ecc984328f6a490f46f45a26d0f3b8
WB Welch <wbwelch@wbs-macbook-pro.attlocal.net>
2017-12-20 01:01:17 -0600

    Hourly divs created, formatting started for sidescroll. Home page design work.

--- assets/javascript/zipcode.js
+++ assets/javascript/zipcode.js
@@ -13,20 +13,23 @@ function displayWeatherInfo(zipcode) {
     	for (var i = 0; i < 10; i++){
 //storing the forecast data
    		 var hour = response.hourly_forecast[i].FCTTIME.civil;
+		var hourDiv = $("<div class='hourDiv'>");
    		 var pOne = $("<p>").text("Time: " + hour);
-  			zipDiv.append(pOne);
+  			hourDiv.append(pOne);
+			
+		 var image = response.hourly_forecast[i].icon_url;
+   		 var pFour = $("<img>").attr("src", image);
+  			hourDiv.append(pFour);
 
  		 var temperature = response.hourly_forecast[i].temp.english;
    		 var pTwo = $("<p>").text("Temperature: " + temperature);
-  			zipDiv.append(pTwo);
+  			hourDiv.append(pTwo);
 
 		 var condition = response.hourly_forecast[i].condition;
    		 var pThree = $("<p>").text("Conditions: " + condition);
-  			zipDiv.append(pThree);
-
-		 var image = response.hourly_forecast[i].icon_url;
-   		 var pFour = $("<img>").attr("src", image);
-  			zipDiv.append(pFour);
+  			hourDiv.append(pThree);
+		zipDiv.append(hourDiv);
+		
 }
 //putting zipcode info in
    		$("#weather").append(zipDiv);
