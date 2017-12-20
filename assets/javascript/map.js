5e28eae027412a1ea10a8565c02a2e6cc5bf540e
WB Welch <wbwelch@wbs-macbook-pro.attlocal.net>
2017-12-20 02:41:40 -0600

    Map psuedocode added. Working on lat and long api call.

--- assets/javascript/map.js
+++ assets/javascript/map.js
@@ -1,3 +1,6 @@
+
+var latitude = "";
+var longitude = "";
 var map, infoWindow;
         function initMap() {
           map = new google.maps.Map(document.getElementById('map'), {
@@ -11,9 +14,13 @@ var map, infoWindow;
             navigator.geolocation.getCurrentPosition(function(position) {
               var pos = {
                 lat: position.coords.latitude,
-                lng: position.coords.longitude
+                lng: position.coords.longitude,
+				  
               };
-
+				longitude = position.coords.longitude;
+				latitude = position.coords.latitude;
+				console.log(latitude);
+				console.log(longitude);
               infoWindow.setPosition(pos);
               infoWindow.setContent('Location found.');
               infoWindow.open(map);
@@ -25,7 +32,7 @@ var map, infoWindow;
             // Browser doesn't support Geolocation
             handleLocationError(false, infoWindow, map.getCenter());
           }
-        }
+        };
 
         function handleLocationError(browserHasGeolocation, infoWindow, pos) {
           infoWindow.setPosition(pos);
@@ -33,4 +40,23 @@ var map, infoWindow;
                                 'Error: The Geolocation service failed.' :
                                 'Error: Your browser doesn\'t support geolocation.');
           infoWindow.open(map);
-        }
+        };
+
+//working on code to convert lat and long to physical address. How do I make it wait to locate lat and long before running this function
+//http://techslides.com/convert-latitude-and-longitude-to-a-street-address
+//want to print street address in sidebar beside map
+//function addressConvert() {
+	
+	//var queryUrl = "http://maps.googleapis.com/maps/api/geocode/json?latlng=" + latitude + "," + longitude + "&sensor=false"
+
+	//$.ajax({
+			//url: queryURL,
+			//method: "GET"
+		//  }).done(function(response) {
+				//console.log(respone);
+		//	});
+	//};
+
+//addressConvert();
+
+	
