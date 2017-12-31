2574e3ea50be3f778311efd58df3af6bb4dd79b2
WB Welch <wbwelch@wbs-macbook-pro.attlocal.net>
2017-12-30 23:03:56 -0600

    YouTube played embeded with playlist search function.

--- assets/javascript/youtube.js
+++ assets/javascript/youtube.js
@@ -1,36 +1,9 @@
-// JavaScript Document
 
-var tag = document.createElement('script');
-
-      tag.src = "https://www.youtube.com/iframe_api";
-      var firstScriptTag = document.getElementsByTagName('script')[0];
-      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-
-      var player;
-      function onYouTubeIframeAPIReady() {
-        player = new YT.Player('player', {
-          height: '390',
-          width: '640',
-          videoId: 'M7lc1UVf-VE',
-          events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
-          }
-        });
-      }
-
-      function onPlayerReady(event) {
-        event.target.playVideo();
-      }
-
-      var done = false;
-      function onPlayerStateChange(event) {
-        if (event.data == YT.PlayerState.PLAYING && !done) {
-          setTimeout(stopVideo, 6000);
-          done = true;
-        }
-      }
-      function stopVideo() {
-        player.stopVideo();
-      }
+function go_get(){
+ var base_url = 'http://www.youtube.com/embed?listType=search&list=';
+ var search_field = document.getElementById('yourtextfield').value;
+ var target_url = base_url + search_field;
+ var ifr = document.getElementById('youriframe');
+ ifr.src = target_url;
+ return false;
+}
