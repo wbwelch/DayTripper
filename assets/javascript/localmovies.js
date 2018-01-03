4849456e6ac0f4e3103a617268801d74eda64cd2
WB Welch <wbwelch@wbs-macbook-pro.attlocal.net>
2018-01-02 21:31:36 -0600

    Local movie showtimes API work

--- assets/javascript/localmovies.js
+++ assets/javascript/localmovies.js
@@ -3,7 +3,7 @@
 var zipcode = "";
 
 function localMovieSearch() { 
-	var queryURL = "http://data.tmsapi.com/v1.1/movies/showings?startDate=" + "2018-01-01" + "&zip=" + "75206" + "&api_key=guyv9by6h494tz4s9xfvpqrq";
+	var queryURL = "http://data.tmsapi.com/v1.1/movies/showings?startDate=" + "2018-01-02" + "&zip=" + zipcode + "&api_key=guyv9by6h494tz4s9xfvpqrq";
 	
 	
 
@@ -13,31 +13,45 @@ function localMovieSearch() {
       		}).done(function(response) {
 		  		console.log(response);
 			
-			for (var i = 0; i < response.length; i++) {
+			for (var i = 0; i < 15; i++) {
 				var movieName = response[i].title;
-				var runtime = response[i].runtime;
-				//var moviePoster = response[i].preferredimage.uri;
+				var runtime = response[i].runTime;
+				var moviePoster = response[i].preferredImage.uri;
 				var rating = response[i].ratings[0].code;
+				var topCast = response[i].topCast;
+				var summary = response[i].shortDescription;
 				
 				
 				var nowPlayingDiv = $("<div class = 'dynCard'>");
 				
-				var pOne = $("<p>").html("<h2 class='wordBreak'>" + movieName + "</h2>");
+				var pOne = $("<div>").html("<h2 class='wordBreak movie-title'>" + movieName + "</h2>");
 				nowPlayingDiv.append(pOne);
-				//var pTwo = $("<img class='movieImage'>").attr("src", moviePoster);
-				//nowPlayingDiv.append(pTwo);
-				var pThree = $("<p>").html("<h2 class='wordBreak'>" + runtime + "</h2>");
+				var pTwo = $("<p class='wordBreak'>").html("<em style='color:white;'>" + summary + "</em>");
+				nowPlayingDiv.append(pTwo);
+				var pThree = $("<h2 class='wordBreak'>").html(runtime[2,3] + " hour(s) " + runtime[5,6] + " min.");
 				nowPlayingDiv.append(pThree);
-				var pFour = $("<p>").html("<h2 class='wordBreak'>" + rating + "</h2>");
+				var pFour = $("<p style='color:white' class='wordBreak'>").html("<strong>" + rating + "</strong>");
 				nowPlayingDiv.append(pFour);
 				
+				for (var k = 0; k < 3; k++) {
+					var pFive = $("<p class='wordBreak small'>").html(topCast[k]);
+					nowPlayingDiv.append(pFive);
+				};
 				
-				for (var s = 0; s < 10; s++) {
-					var theater = response[i].showtimes[s].theatre.name;
-						console.log(theater);
-					var showtimes = response[i].showtimes[s].dateTime;
+				for (var j = 0; j < 10; j++) {
+					var theater = response[i].showtimes[j].theatre.name;
+						console.log(theater);	
+				};
+				
+				for (var l = 0; l < 10; l++) {
+				var showtimes = response[i].showtimes[l].dateTime;
 						console.log(showtimes);
-				}
+				};
+				
+				var getShowtimesButton = $("<button class='btn' id='movieZipSearch' style='margin-right: 20px; margin-left: 15px'>Get Showtimes</button>");
+				nowPlayingDiv.append(getShowtimesButton);
+				
+				
 				//console.log(movieName);
 				//console.log(runtime);
 				//console.log(moviePoster);
@@ -51,8 +65,10 @@ function localMovieSearch() {
 
 $("#movieZipSearch").on("click", function(event){
 	event.preventDefault();
-    localMovieSearch();
+	$("#movieZipOutput").empty();
 	zipcode = $("#movieZipInput").val().trim();
+	localMovieSearch(zipcode);
+
 });
 		  
 //title
--- assets/javascript/moviestreaming.js
+++ assets/javascript/moviestreaming.js
@@ -2,7 +2,7 @@
 
  
 
-var queryURL = "http://api-public.guidebox.com/v2//v2/search?type=" + "movie" + "&field=title&query=" + "Pacific Rim"
+var queryURL = "http://api-public.guidebox.com/v2/search?api_key=1c94ca8b2662e97be6c424070e65bb62ea5&type=" + "movie" + "&field=title&query=" + "Pacific Rim"
 
       $.ajax({
         url: queryURL,
