afb2ff11413abe97871c85ae273103b8e96d4e09
WB Welch <wbwelch@wbs-macbook-pro.attlocal.net>
2018-01-07 19:09:46 -0600

    Eventful API work

--- assets/javascript/eventful.js
+++ assets/javascript/eventful.js
@@ -44,33 +44,33 @@ function eventfulInfo(){
 
         	   }).done(function(response) {
 		  		console.log(response);
-				console.log(response[0].events[0].event[0]);
+				//console.log(response.events[0].event[0]);
 
 
-		  		//for (var i = 0; i < 9; i++) {
-					//console.log(response[i].event);
-				for (var k = 0; k < 9; k++) {
-					var eventfulDiv= $("<div class = 'dynCard'>");
-					
-					//var eventName = response.events.event[k].title;
-//					var p1= $("<h2 class='wordBreak'>").text(eventName);
-//					eventfulDiv.append(p1);
-//	
-//		  			var date = response[i].start_time;
-//		  			var p2 = $("<p>").text(date);
-//		  			eventfulDiv.append(p2);
-//
-//		  			var venueName = response[i].venue_name;
-//		  			var p3 = $("<p>").text(venueName);
-//		  			eventfulDiv.append(p3);
-//					
-//					var address = response[i].venue_address;
-//		  			var p5 = $("<p>").text(address);
-//		  			eventfulDiv.append(p5);
-//
-//		  			var url = response[i].url;
-//		  			var p4 = $("<p>").text(url);
-//		  			eventfulDiv.append(p4);
+		  		for (var i = 0; i < 9; i++) {
+					console.log(response[i].events);
+					//for (var k = 0; k < 9; k++) {
+						var eventfulDiv= $("<div class = 'dynCard'>");
+
+						//var eventName = response.events.event[k].title;
+	//					var p1= $("<h2 class='wordBreak'>").text(eventName);
+	//					eventfulDiv.append(p1);
+	//	
+	//		  			var date = response[i].start_time;
+	//		  			var p2 = $("<p>").text(date);
+	//		  			eventfulDiv.append(p2);
+	//
+	//		  			var venueName = response[i].venue_name;
+	//		  			var p3 = $("<p>").text(venueName);
+	//		  			eventfulDiv.append(p3);
+	//					
+	//					var address = response[i].venue_address;
+	//		  			var p5 = $("<p>").text(address);
+	//		  			eventfulDiv.append(p5);
+	//
+	//		  			var url = response[i].url;
+	//		  			var p4 = $("<p>").text(url);
+	//		  			eventfulDiv.append(p4);
 					
 					
 					$("#eventfulOutput").append(eventfulDiv);
